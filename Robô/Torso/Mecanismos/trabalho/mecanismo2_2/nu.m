a94f780f445bad5fcc9005ef25411a179c3a6515
franciele19 <francielesy@gmail.com>
2022-04-27 21:43:35 -0300

    Projeto 2022
    Início do projeto 2022

--- Robô/Torso/Mecanismos/trabalho/mecanismo2_2/nu.m
+++
@@ -1,8 +0,0 @@
-function b=nu(x,t)
-l1 = 5;
-l2 =5;
-b = [zeros(18,1);
-     -1;
-     1; 
-     -l1*sin(t)/10;
-     l1*cos(t)/10];
--- Robô/Torso/Mecanismos/trabalho/mecanismo3_2/nu.m
+++
@@ -1,7 +0,0 @@
-function b=nu(x,t)
-l1 = 5;
-l2 =3;
-b = [zeros(13,1);
-     -sin(t)*(l1/4 + l2/4);
-     cos(t)*(l1/4 + l2/4);
-     1];
